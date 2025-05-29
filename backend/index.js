2cc4382ceaa3fe60271da01a5723d332a8a41d60
LBRezede <lucas.contato.fenix@gmail.com>
2025-05-28 21:05:35 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+
+const authRoutes = require('./routes/auth');
+const taskRoutes = require('./routes/tasks');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
 
 app.get('/', (req, res) => {
   res.send('API funcionando!');
