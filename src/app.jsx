2c83a1dd860fb0b1b2a008c13e5f268ef6c2cc74
LBRezede <lucas.contato.fenix@gmail.com>
2025-05-28 21:31:21 -0300

    Add files via upload

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Minha Aplicação React</h1>
+      <p>Projeto de gerenciamento de tarefas</p>
+    </div>
+  );
+}
+
+export default App;
